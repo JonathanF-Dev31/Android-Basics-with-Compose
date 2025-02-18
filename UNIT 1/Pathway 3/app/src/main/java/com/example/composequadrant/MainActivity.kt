bd570183957414e0f9fcba7770c26c876b67c5fb
JonathanF-Dev31 <jonathan.afo31@gmail.com>
2025-02-17 22:58:48 -0500

    Unit 1's Exercises about layouts and Jetpack Compose

---
+++ UNIT 1/Pathway 3/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -0,0 +1,168 @@
+package com.example.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeQuadrantTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeQuadrantApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeQuadrantApp() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+    ){
+        Row (
+            modifier = Modifier.weight(1f),
+        ){
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFEADDFF)),
+                contentAlignment = Alignment.Center
+            ){
+                Column {
+                    Text (
+                        text = "Text composable",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text (
+                        text = "Displays text and follows the recommended Material Design guidelines.",
+                        modifier = Modifier
+                            .padding(16.dp),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFD0BCFF)),
+                contentAlignment = Alignment.Center
+            ){
+                Column {
+                    Text (
+                        text = "Image composable",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text (
+                        text = "Creates a composable that lays out and draws a given Painter class object.",
+                        modifier = Modifier
+                            .padding(16.dp),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+        }
+        Row (
+            modifier = Modifier
+                .weight(1f)
+
+        ){
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFB69DF8)),
+                contentAlignment = Alignment.Center
+            ){
+                Column {
+                    Text (
+                        text = "Row composable",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text (
+                        text = "A layout composable that places its children in a horizontal sequence.",
+                        modifier = Modifier
+                            .padding(16.dp),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFF6EDFF)),
+                contentAlignment = Alignment.Center
+            ){
+                Column {
+                    Text (
+                        text = "Column composable",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text (
+                        text = "A layout composable that places its children in a vertical sequence.",
+                        modifier = Modifier
+                            .padding(16.dp),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrantAppPreview() {
+    ComposeQuadrantTheme {
+        ComposeQuadrantApp()
+    }
+}
