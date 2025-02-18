cdc93852e8407c4adc6afc937d3e37ab8d748a89
JonathanF-Dev31 <jonathan.afo31@gmail.com>
2025-02-17 23:08:26 -0500

    Exercises of Course's Unit 1 using Jetpack Compose and Layouts

--- UNIT 1/Pathway 3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ UNIT 1/Pathway 3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,16 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
